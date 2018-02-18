c4eb392bcdde4576c3aa9eda5073aef53ee7a183
Aaron Williams <aaronbenwilliams@gmail.com>
2018-02-18 17:18:41 +0100

    Add files via upload

---
+++ lolninja_disable.bat
@@ -0,0 +1,5 @@
+ECHO Why are you doing this? It's not going to be enjoyable 
+PAUSE
+netsh advfirewall firewall delete rule name="lolchat"
+ECHO Good luck scrub...
+PAUSE
---
+++ lolninja_enable.bat
@@ -0,0 +1,3 @@
+netsh advfirewall firewall add rule name="lolchat" dir=out remoteip=185.40.64.69 protocol=TCP action=block
+ECHO Ninja mode enabled!
+PAUSE
